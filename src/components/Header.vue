c94850d48f6d055271c25c2879c82c009a8100ef
Andrea Prina <and.prina@gmail.com>
2022-07-26 16:26:17 +0200

    feat(Header): Add full screen hero image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,19 @@
     <header>
         <SocialBar
         :contacts = "contacts"/>
-        <div>
-            <nav>
-                <!-- Navbar -->
-            </nav>
-            <div>
-                <!-- Hero text -->
+        <div class="main-header">
+            <img class="header-img" src="../assets/img/hero-image.png" alt="">
+
+            <div class="header-items wrapper">
+                <nav>
+                    NAVBAR
+                    <!-- Navbar -->
+                </nav>
+                <div class="hero-text">
+                    HERO TEXT
+                </div>
             </div>
+
         </div>
     </header>
 </template>
@@ -33,6 +39,31 @@ export default {
 <style lang="scss">
 @import '../assets/styles/generals.scss';
 @import '../assets/styles/variables.scss';
+
+    .main-header {
+        height: 400px;
+        position: relative;
+    }
+
+    .header-img {
+        filter: brightness(0.5) saturate(75%);
+        object-fit: cover;
+        width: 100%;
+    }
+
+    .header-items {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
+    .hero-text {
+        text-align: center;
+        padding: 8rem;
+        border: 1px solid red;
+    }
+
 
 
 </style>

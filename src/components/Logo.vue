69a38d5e2f15b488e41e463c4f786d7b01b1b086
Andrea Prina <and.prina@gmail.com>
2022-07-26 16:57:23 +0200

    feat(Logo): Create logo draft style

---
+++ src/components/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="logo">
+        <span class="colored-logo">NEX</span>GEN
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '../assets/styles/variables.scss';
+
+    .logo{
+        color: white;
+    }
+
+    .colored-logo {
+        background-color: $lightAccentColor;
+        padding: 0.5rem 0rem 0.5rem 1.5rem;
+        border-radius: 20px 0px 0 20px;
+    }
+
+</style>

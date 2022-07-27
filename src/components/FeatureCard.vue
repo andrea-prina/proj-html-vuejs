010fe2261095074e9463b2ef561f3c428ab0b542
Andrea Prina <and.prina@gmail.com>
2022-07-27 12:02:09 +0200

    feat(FeatureCard): Create component to display info in card style

---
+++ src/components/FeatureCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card three-cols-card">
+        <div class="card-icon centered-circle-icon big-icon">
+            <i class="fa-brands fa-facebook"></i>
+        </div>
+        <h3>Title</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '../assets/styles/generals.scss';
+@import '../assets/styles/variables.scss';
+
+.card {
+
+    border: 1px solid yellow;
+
+    .card-icon, h3, p {
+        margin-bottom: 1rem;
+    }
+}
+
+.card-icon {
+    background-color: red;
+}
+
+</style>
